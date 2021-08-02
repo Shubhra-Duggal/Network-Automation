ce2f109d34b937e54f93077c07aa41ddb670a3e0
sarvgyata <42912140+Shubhra-Duggal@users.noreply.github.com>
2021-08-02 08:39:10 +0530

    Update python.py

--- Code/python.py
+++ Code/python.py
@@ -9,11 +9,9 @@ import pexpect
 c = pexpect.spawnu('/usr/bin/env python')
 
 c.expect('>>>')
-print('And now for something completely different...')
 print(''.join(reversed((c.before))))
-print('Yes, it\'s python, but it\'s backwards.')
+print('Output is reversed.')
 print()
-print('Escape character is \'^]\'.')
 print(c.after, end=' ')
 c.interact()
 c.kill(1)
