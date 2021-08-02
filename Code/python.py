44e9399dbadd096ed025d147e04659bc64460d98
sarvgyata <42912140+Shubhra-Duggal@users.noreply.github.com>
2021-08-02 18:17:30 +0530

    Update python.py

--- Code/python.py
+++ Code/python.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import pexpect
 
 c = pexpect.spawnu('/usr/bin/env python')
